119b7a74018f475e18354986bc2aeb5ae9029c05
luuribe <luuribe@bancolombia.com.co>
2023-05-12 19:25:39 -0500

    cambios

--- index.js
+++ index.js
@@ -15,19 +15,90 @@ class Calculadora {
     }
 
     dividir (num1,num2) {
-        return num1 / num2;
+        if (num1==0 && num2==0) {
+            return 'Jueputaaaa como que 0/0 cabeza pinga bobo tonto';
+        } else {
+            return num1 / num2;
+        }
     }
 }
 
-const calculadora = new Calculadora ();
+const calculadora = new Calculadora();
+const pantalla = document.getElementsByClassName("pantalla");
+let num1 = 0;
+let num2 = 0;
+let operador;
+let aux = false;
 
+function registrarNumero() {
+    const keys = document.querySelector('.calculadora')
+    if (num1==0){
+        keys.addEventListener('click', e => {
+        if (e.target.matches('button')) {
+            const key = e.target;
+            num1 = key.textContent;
+            pantalla.value = num1;
+        }
+        })
+    } else {
+        keys.addEventListener('click', e => {
+        if (e.target.matches('button')) {
+            const key = e.target;
+            num2 = key.textContent;
+            pantalla.value = num2;
+        }
+        })
+    }
+}
+
+function sumar() {
+    operador = '+';
+}
+
+function restar() {
+    operador = '-';
+}
+
+function multiplicar() {
+    operador = '*';
+}
+
+function dividir() {
+    operador = '/';
+}
+
+function borrar() {
+    pantalla.value = '0';
+}
+
+function resultado() {
+    switch (operador) {
+        case '+':
+            pantalla.value = calculadora.sumar(num1, num2);
+            break;
+        case '-':
+            pantalla.value = calculadora.sumar(num1, num2);
+            break;
+        case '*':
+            pantalla.value = calculadora.sumar(num1, num2);
+            break;
+        case '/':
+            pantalla.value = calculadora.sumar(num1, num2);
+            break;
+        case 'default':
+            pantalla.value = '0';
+            break;
+    }
+}
+
+/*
 class Pantalla {
-    constructor(pantallaValorAnterior, pantallaValorActual){
+    constructor(pantallaValorAnterior, pantallaValorActual) {
         this.pantallaValorActual = pantallaValorActual;
         this.pantallaValorAnterior = pantallaValorAnterior;
-        this.canculadora = new Calculadora ();
+        this.calculadora = new Calculadora ();
         this.valorActual = '';
-        this.valorAnterios = '';
+        this.valorAnterior = '';
     }
 
     agregarNumero(numero) {
@@ -35,7 +106,11 @@ class Pantalla {
     }
 
 }
+const pantalla = pantalla(pantallaValorAnterior, pantallaValorActual);
 
 botonesNumeros.forEach(boton => {
-    boton.addEventlistener('click', ()=>{
+    boton.addEventlistener('click', ()=> pantalla.agregarNumero(boton.innerHTML))
 });
+
+imprimirValores
+*/
